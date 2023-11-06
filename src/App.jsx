e7be42d4d7c86876bb0443f2f23fca199f23ae6e
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-06 14:53:01 -0300

    Criada a função que adiciona o valor do target (cor) no estado da cor caso a classe no input esteja com o valor checked, se não tiver checkado é pra retirar o valor do target da array fazendo o filtro da mesma

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 const App = () => {
   const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor != target.value));
+    }
+  }
+
   return;
 };
 
